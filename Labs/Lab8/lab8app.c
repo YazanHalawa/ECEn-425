7623fbe641ba6fdf87168c8d32b8e924e2214f4a
Yazan Halawa <y.halawa94@gmail.com>
2015-11-30 20:11:39 -0700

    finished stats task

--- Labs/Lab8/lab8app.c
+++ Labs/Lab8/lab8app.c
@@ -20,16 +20,53 @@ int placement[TASK_STACK_SIZE];     /* a stack for each task */
 int communication[TASK_STACK_SIZE];
 int statistics[TASK_STACK_SIZE];
 
-void placementTask(void){
+void placementTask(void){ /* Determines sequence of slide and rotate commands */
 
 }
 
-void communicationTask(void){
+void communicationTask(void){ /* Handles communication with Simptris */
 
 }
 
-void statisticsTask(void){
+void statisticsTask(void){ /* tracks statistics */
+    unsigned idleCount, max;
+
+    YKDelayTask(1);
+    printString("Welcome to the YAK kernel\r\n");
+    printString("Determining CPU capacity\r\n");
+    YKDelayTask(1);
+    YKIdleCount = 0;
+    YKDelayTask(5);
+    max = YKIdleCount / 25;
+    YKIdleCount = 0;
+
+    // Create the tasks here
+    YKNewTask(placement, (void*) &placement[TASK_STACK_SIZE], 1);
+    YKNewTask(communication, (void*) &communication[TASK_STACK_SIZE], 2);
+
+    while(1){
+
+        YKDelayTask(20);
+        
+        YKEnterMutex();
+        switchCount = YKCtxSwCount;
+        idleCount = YKIdleCount;
+        YKExitMutex();
+        
+        printString("<<<<< Context switches: ");
+        printInt((int)switchCount);
+        printString(", CPU usage: ");
+        tmp = (int) (idleCount/max);
+        printInt(100-tmp);
+        printString("% >>>>>\r\n");
+        
+        YKEnterMutex();
+        YKCtxSwCount = 0;
+        YKIdleCount = 0;
+        YKExitMutex();
+    }
     
+
 }
 
 
@@ -37,9 +74,7 @@ void main(void)
 {
     YKInitialize();
 
-    charEvent = YKEventCreate(0);
-    numEvent = YKEventCreate(0);
-    YKNewTask(STask, (void *) &STaskStk[TASK_STACK_SIZE], 0);
+    YKNewTask(statisticsTask, (void *) &statistics[TASK_STACK_SIZE], 0);
     
     YKRun();
 }
